541960459247b77fde487678f8d761ff7f117308
BORDOTEO <bordonematteo@yahoo.it>
2025-05-17 23:36:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 3000;
 // 🔁 Sostituisci con il tuo URL Google Apps Script:
 const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/TUO_SCRIPT_ID/exec';
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://6828fffeb83131a407238bd8--tubular-dragon-c806bc.netlify.app/',
+  methods: ['POST']
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/api/proxy', async (req, res) => {
